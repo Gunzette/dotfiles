fd059231960895eab61bd44ec34af035d56b63bd
Gunzette <gunzettedev@gmail.com>
2024-08-24 16:41:05 +0200

    made shell actually usable (kinda)

---
+++ .config/fish/config.fish
@@ -0,0 +1,32 @@
+#
+# Gunzette's Fish Config
+#
+
+### EXPORTS
+
+set fish_greeting	# disables fish intro message
+set EDITOR "vim"
+set VISUAL "Kate"	# nothing better installed rn :(
+
+#TODO: manpager
+
+#TODO: colors
+
+### ALIASES
+
+# added flags
+alias df="df -h"	# readable sizes
+alias grep="grep --color=auto"	# colorized grep
+
+# git
+alias gcommit="git commit -m"	# commit with message
+alias gclone="git clone"	# clone git repo
+alias ghclone="gh repo clone"	# clone github repo
+#TODO: more
+
+
+### STARTUP
+
+fastfetch
+
+starship init fish | source
--- useconfig.sh
+++ useconfig.sh
@@ -3,8 +3,11 @@
 echo "trying to create backup directory:"
 mkdir ~/.dotfiles.bak
 mkdir ~/.dotfiles.bak/.config
+
 cp -rfv ~/.config/fish ~/.dotfiles.bak/.config
+cp -rfv ~/.config/starship.toml ~/.dotfiles.bak/.config
 
 cp -rfv .config/fish ~/.config
+cp -rfv .config/starship.toml ~/.config
 
 echo "success"
