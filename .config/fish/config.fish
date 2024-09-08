370d1104493e8c94b8f3e6d9149b02a0b7759874
Gunzette <gunzettedev@gmail.com>
2024-09-08 10:25:52 +0200

    added a few hyprland keybinds

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -52,4 +52,9 @@ alias clear="clear && fastfetch"
 fastfetch
 
 fzf --fish | source
+
+#function starship_transient_prompt_func
+#  starship module character
+#end
 starship init fish | source
+#enable_transience
